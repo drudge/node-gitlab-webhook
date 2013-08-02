aab9c450e6ab4347cb8ab7220e69de8eea9cc54f
Nicholas Penree <nick@penree.com>
2013-08-01 22:56:54 -0400

    fixups

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 'use strict';
 
-var util = require('util').isArray;
+var isArray = require('util').isArray;
 var exec = require('child_process').exec;
 var debug = require('debug')('gitlab-webook-handler')
 
@@ -16,22 +16,37 @@ exports = module.exports = function(opts) {
     var token = req.param(param);
     var payload = req.body || {};
     var branches = opts.branches || [];
+    var ref = payload.ref;
+    var cmd = opts.exec || 'git pull ' + ref;
     
     if (token !== opts.token) {
       debug('%s "%s" does not match, denying access', param, token);
-      return res.status(403);
+      res.status(404);
+      return res.end();
     }
-    
     branches = isArray(branches)? branches : [branches];
     
-    if (branches.indexOf('*') === -1 || branches.indexOf(payload.ref) === -1) {
-      debug('%s not found in allowed branches: %s', branches.join(', '));
-      return res.status(403);
+    if (branches.indexOf('*') === -1 && branches.indexOf(ref) === -1) {
+      debug('%s not found in allowed branches: %s', ref, branches.join(', '));
+      res.status(403);
+      return res.end();
     }
     
-    debug('%s allowed', payload.ref);
-    exec('git pull ' + payload.ref, function() {
-      res.send(200);
-    });
+    debug('%s allowed', ref);
+    try {
+      exec(cmd, function(err, stdout, stderr) {
+        if (err) {
+          debug('error executing `' + cmd + '`: ' + err.message);
+          res.send(500);
+          return res.end();
+        }
+        
+        res.send(200);
+        return res.end();
+      });
+    } catch (err) {
+      res.send(500);
+      return res.end();
+    }
   };
 };
